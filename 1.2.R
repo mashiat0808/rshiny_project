5274849663686ee9f2cffcaf108aee0b01b9a0c6
mashiat0808 <59837488+mashiat0808@users.noreply.github.com>
2021-02-22 00:29:25 +0600

    Add files via upload
    issues: rshiny doesnt print the plot

---
+++ 1.2.R
@@ -0,0 +1,57 @@
+library(shiny)
+library(dplyr)
+library(tidyverse)
+library(ggplot2)
+
+
+#dataframe manipulation
+country.data <-read.csv(file.choose())
+country.data <- country.data[,c(1,3,8)]
+
+#country.data <- country.data %>% spread(country, daily_vaccinations)
+country.data[is.na(country.data)]=0
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    
+    # Application title
+    titlePanel("Number of vaccinations daily based on country"),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            selectInput(inputId = "region", label= "Country:", 
+                        choices= as.factor(country.data$country) %>% levels() %>% as.list, selected = "Albania"),
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel(
+            plotlyOutput(outputId = "distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) ({
+    
+    
+    output$distplot <- renderPlotly({ 
+        
+        data <- subset(country.data, country==inputId$region)
+        
+       
+        mn <-plot_ly(data, x = data$date, y = data$daily_vaccinations, type= 'scatter', mode= 'lines+markers',line = list(color = 'rgb(205, 12, 24)'))
+        mn<- mn %>%  layout(title = "Number of Vaccinations done by Selected Country per day", xaxis = list(title = "Dates"), yaxis = list (title = "Numbers"))
+        return(mn)
+        
+        
+    })
+    
+    
+})
+
+
+
+
+# Run the application 
+shinyApp(ui = ui, server = server)
